904a1c7287bf36ae9e7942490fce51307eb2a9ff
Brent Miller <brent.miller2@gmail.com>
2018-10-09 13:44:22 -0400

    Add basic template and styling to Sidebar Component
    Add the bare bones to the template and style of the Sidebar Component. Add simple open and close functionality based on "mouseenter" and "mouseleave" events.

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -1,7 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'ng-icon-sidebar',
-  template: ``
+  template: `
+    <aside aria-label="Icon Sidebar"
+           class="ng-icon-sidebar ng-icon-sidebar--{{opened ? 'opened' : 'closed'}}"
+           (mouseenter)="onMouseEnter()"
+           (mouseleave)="onMouseLeave()"
+           role="complementary">
+      <ng-content></ng-content>
+    </aside>
+  `,
+  styles: [`
+    .ng-icon-sidebar {
+      bottom: 0;
+      left: 0;
+      height: 100%;
+      overflow-x: hidden;
+      overflow-y: auto;
+      position: absolute;
+      top: 0;
+      transition: 0.25s;
+      z-index: 99999999;
+    }
+
+    .ng-icon-sidebar--closed {
+      width: 40px;
+    }
+
+    .ng-icon-sidebar--opened {
+      box-shadow: 0 0 4px rgba(0, 0, 0, 0.1);
+      width: 336px;
+    }
+  `],
+  encapsulation: ViewEncapsulation.None
 })
-export class SidebarComponent {}
+export class SidebarComponent {
+  public opened = false;
+
+  public onMouseEnter(): void {
+    this.opened = true;
+  }
+
+  public onMouseLeave(): void {
+    this.opened = false;
+  }
+}
