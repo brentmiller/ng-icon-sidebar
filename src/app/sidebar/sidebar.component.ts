8713fe7315ad5c4f4ef3eb936f8dcb63bb85e30f
Brent Miller <brent.miller2@gmail.com>
2018-10-30 10:34:40 -0400

    Add keyboard interaction to sidebar
    Adds logic to the Sidebar Component for handling keyboard interaction. The sidebar now opens and closes in reaction to the sidebar's contents receiving and losing keyboard focus.

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -4,45 +4,67 @@ import { Component, ViewEncapsulation } from '@angular/core';
   selector: 'ng-icon-sidebar',
   template: `
     <aside aria-label="Icon Sidebar"
-           class="ng-icon-sidebar ng-icon-sidebar--{{ opened ? 'opened' : 'closed' }}"
+           class="ng-icon-sidebar ng-icon-sidebar--{{ isOpen ? 'opened' : 'closed' }}"
+           (focusin)="onFocusIn()"
+           (focusout)="onFocusOut()"
            (mouseenter)="onMouseEnter()"
            (mouseleave)="onMouseLeave()"
            role="complementary">
       <ng-content></ng-content>
     </aside>
   `,
-  styles: [`
-    .ng-icon-sidebar {
-      bottom: 0;
-      left: 0;
-      height: 100%;
-      overflow-x: hidden;
-      overflow-y: auto;
-      position: absolute;
-      top: 0;
-      transition: 0.25s;
-      z-index: 99999999;
-    }
+  styles: [
+    `
+      .ng-icon-sidebar {
+        bottom: 0;
+        left: 0;
+        height: 100%;
+        overflow-x: hidden;
+        overflow-y: auto;
+        position: absolute;
+        top: 0;
+        transition: 0.25s;
+        z-index: 99999999;
+      }
 
-    .ng-icon-sidebar--closed {
-      width: 48px;
-    }
+      .ng-icon-sidebar--closed {
+        width: 48px;
+      }
 
-    .ng-icon-sidebar--opened {
-      box-shadow: 0 0 4px rgba(0, 0, 0, 0.1);
-      width: 256px;
-    }
-  `],
+      .ng-icon-sidebar--opened {
+        box-shadow: 0 0 4px rgba(0, 0, 0, 0.1);
+        width: 256px;
+      }
+    `
+  ],
   encapsulation: ViewEncapsulation.None
 })
 export class SidebarComponent {
-  public opened = false;
+  public isOpen = false;
+  private isFocusIn = false;
+  private isMouseIn = false;
+
+  public onFocusIn(): void {
+    this.isFocusIn = true;
+    this.isOpen = true;
+  }
+
+  public onFocusOut(): void {
+    this.isFocusIn = false;
+    if (!this.isMouseIn) {
+      this.isOpen = false;
+    }
+  }
 
   public onMouseEnter(): void {
-    this.opened = true;
+    this.isMouseIn = true;
+    this.isOpen = true;
   }
 
   public onMouseLeave(): void {
-    this.opened = false;
+    this.isMouseIn = false;
+    if (!this.isFocusIn) {
+      this.isOpen = false;
+    }
   }
 }
